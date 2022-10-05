1daeba007057e71746b545847b9240414efd6f1a
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-10-05 12:52:55 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.82 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Array.Sort(A);
+        Array.Sort(B, (a, b) => {
+           if(a > b)
+               return -1;
+            else if(a < b)
+                return 1;
+            else
+                return 0;
+        });
+        
+        for(int i = 0; i < A.Length; i++)
+        {
+            answer += A[i] * B[i];
+        }
+        
+        return answer;
+    }
+}
