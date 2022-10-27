9aec5338684438041ee815e5e040bf0015dc45ac
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-10-27 14:53:24 +0900

    [Silver IV] Title: 수 찾기, Time: 220 ms, Memory: 26876 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -5,30 +5,39 @@ int N = int.Parse(Console.ReadLine());
         int[] A = Console.ReadLine().Split().Select(int.Parse).ToArray();
         int M = int.Parse(Console.ReadLine());
         int[] B = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        
-        Array.Sort(A);
 
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < B.Length; i++)
+        Array.Sort(A);
+
+        for (int i = 0; i < M; i++)
         {
-            sb.AppendLine(Search(A, B[i]));
+            int num = B[i], start = 0, end = N - 1, mid = 0;
+            bool check = false;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                
+                if (A[mid] < num)
+                {
+                    start = mid + 1;
+                }
+                else if (A[mid] > num)
+                {
+                    end = mid - 1;
+                }
+                else if (A[mid] == num)
+                {
+                    sb.AppendLine("1");
+                    check = true;
+                    break;
+                }
+            }
+
+            if (!check)
+            {
+                sb.AppendLine("0");
+            }
         }
         
         Console.WriteLine(sb.ToString());
-        
-        string Search(int[] A, int numB)
-        {
-            int i = A[A.Length / 2] <= numB ? A.Length / 2 : 0;
-            int upper = A[A.Length / 2] <= numB ? A.Length : A.Length / 2;
-
-            for (i = i; i < upper; i++)
-            {
-                if (A[i] == numB)
-                {
-                    return "1";
-                }
-            }
-            
-            return "0";
-        }
