2c2dd255c403b5e37e9989ef19d034c09333f336
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-10-27 09:55:09 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 5708 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cs
+++ 백준/Silver/11047. 동전 0/동전 0.cs
@@ -1,26 +1,24 @@
 using System;
 
 int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        int N = input[0];
-        int K = input[1];
+int N = input[0];
+int K = input[1];
+int[] Coin = new int[N];
 
-        int[] coin = new int[N];
-        for (int i = N - 1; i >= 0; i--)
-        {
-            coin[i] = int.Parse(Console.ReadLine());
-        }
+for (int i = N - 1; i >= 0; i--)
+{
+    Coin[i] = int.Parse(Console.ReadLine());
+}
 
-        int idx = 0;
-        int money = K;
-        int cnt = 0;
+int cnt = 0, idx = 0;
+while (K > 0)
+{
+    if (K / Coin[idx] > 0)
+    {
+        cnt += K / Coin[idx];
+        K = K % Coin[idx];
+    }
+    idx++;
+}
 
-        while (money > 0)
-        {
-            if (money / coin[idx] > 0)
-            {
-                cnt += money / coin[idx];
-                money = money % coin[idx];
-            }
-            idx++;
-        }
-        Console.WriteLine(cnt);
+Console.WriteLine(cnt);
