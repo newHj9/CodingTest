ffe9f014c46c0888cccdd6f34d7266d3b33f2869
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-10-27 09:45:17 +0900

    [Bronze II] Title: 손익분기점, Time: 64 ms, Memory: 5704 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.cs
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cs
@@ -1,18 +1,17 @@
 using System;
 
 int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-long cnt = -1;
 
-if (input[1] < input[2])
+int A = input[0];
+int B = input[1];
+int C = input[2];
+
+if (B >= C)
 {
-    long result = -1;
-
-    result = input[2] - input[1];
-    cnt = input[0] / result;
-
-    Console.WriteLine(cnt + 1);
+    Console.WriteLine(-1);
 }
-else if(input[1] >= input[2])
+else
 {
-    Console.WriteLine(cnt);
+    int result = (A / (C - B)) + 1;
+    Console.WriteLine(result);
 }
