c68b759453251a430acea16448d921783aa08c23
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-11-14 18:49:05 +0900

    [Bronze II] Title: 분산처리, Time: 112 ms, Memory: 6136 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cs
+++ 백준/Bronze/1009. 분산처리/분산처리.cs
@@ -2,10 +2,9 @@ int T = int.Parse(Console.ReadLine());
         
 for (int i = 0; i < T; i++)
 {
-    int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-    string strNum = input[0].ToString();
-    char ch = strNum[strNum.Length - 1];
+    string[] input = Console.ReadLine().Split().ToArray();
+    string oneNum = input[0];
+    char ch = oneNum[oneNum.Length - 1];
 
     if (ch == '0')
     {
@@ -17,22 +16,20 @@ for (int i = 0; i < T; i++)
     }
     else
     {
+        int twoNum = int.Parse(input[1]);
+
         List<int> val = new List<int>();
         int num = 1;
-        for (int j = 0; j < input[1]; j++)
+        for (int j = 0; j < twoNum; j++)
         {
             num *= ch - '0';
             string str = num.ToString();
             if (!val.Contains(str[str.Length - 1] - '0'))
-            {
                 val.Add(str[str.Length - 1] - '0');
-            }
             else
-            {
                 break;
-            }
         }
 
-        Console.WriteLine(val[input[1] % val.Count == 0 ? val.Count - 1 : input[1] % val.Count - 1]);
+        Console.WriteLine(val[twoNum % val.Count == 0 ? val.Count - 1 : twoNum % val.Count - 1]);
     }
 }
