518806d90e5aa5bf0239b444cfc8f00119601349
newHj9 <108648161+newHj9@users.noreply.github.com>
2022-11-15 10:12:16 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cs
+++ 백준/Silver/1026. 보물/보물.cs
@@ -1,18 +1,27 @@
 int N = int.Parse(Console.ReadLine());
 int[] A = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int[] B = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int[] S = new int[N];
 
 List<int> aList = A.ToList();
-List<int> bList = B.ToList();
-
 aList.Sort();
-bList.Sort();
-bList.Reverse();
 
+int result = 0;
+bool[] checkB = new bool[N];
 for (int i = 0; i < N; i++)
 {
-    S[i] = aList[i] * bList[i];
+    int max = -1, maxidx = 0;
+
+    for (int j = 0; j < N; j++)
+    {
+        if (max < B[j] && !checkB[j])
+        {
+            max = B[j];
+            maxidx = j;
+        }
+    }
+
+    result += aList[i] * B[maxidx];
+    checkB[maxidx] = true;
 }
 
-Console.WriteLine(S.Sum());
+Console.WriteLine(result);
